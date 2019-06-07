cf05256638d9e969070cc52e9efd590162d3035e
fonces <fonces.xyz@gmail.com>
2019-06-07 17:36:04 +0900

    fix withLazy types

--- src/with-lazy.tsx
+++ src/with-lazy.tsx
@@ -1,8 +1,7 @@
-import createUseLazy, { UseLazy, CreateUseLazyOptions } from './create-use-lazy'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import React, { ComponentType, ReactElement } from 'react'
+import createUseLazy, { CreateUseLazyOptions, UseLazy } from './create-use-lazy'
 
-type Omit<T, K extends string | number | symbol> = { [P in Exclude<keyof T, K>]: T[P]; }
 type PureProps<P extends LazyComponentProps> = Omit<P, keyof LazyComponentProps>
 
 export type UseLazy = UseLazy
@@ -11,12 +10,13 @@ export type LazyComponentProps = {
   useLazy: UseLazy
 }
 
-function withLazy<P extends LazyComponentProps>(Component: ComponentType<PureProps<P>>, options?: CreateUseLazyOptions) {
+function withLazy<P extends LazyComponentProps>(Component: ComponentType<P>, options?: CreateUseLazyOptions) {
   const useLazy = createUseLazy(options)
   const displayName = `withLazy(${Component.displayName || Component.name})`
-  const UseLazyComponent = (props: PureProps<P>): ReactElement => (
-    <Component {...props} useLazy={useLazy} />
-  )
+  const UseLazyComponent = (props: PureProps<P>): ReactElement => {
+    (props as P).useLazy = useLazy
+    return <Component {...props as P} />
+  }
 
   UseLazyComponent.displayName = displayName
   UseLazyComponent.WrappedComponent = Component
